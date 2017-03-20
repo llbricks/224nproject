8c26c17dcb54fd8b1186715b34cd0a51047f77c4
Alexandre <robicquet.alexandre@gmail.com>
2017-03-19 19:11:10 -0600

    fix train

--- code/data_util.py
+++ code/data_util.py
@@ -10,10 +10,10 @@ def get_word2embed_dict(embedding, vocab):
     Load word vector mapping using @embedding, @vocab.
     Assumes each line of the vocab file matches with those of the embedding
     file.
-    """  
+    """
     embd_size = len(embedding[0])
     ret = OrderedDict()
-    for key in vocab.keys(): 
+    for key in vocab.keys():
         ret[key] = embedding[vocab[key]]
     ret[UNK] = [0]*embd_size
     return ret
@@ -33,8 +33,8 @@ def preprocess_sequence_data(dataset, embed_dict, question_max_length, context_m
         question_data, question_mask = pad(question_embed, n_features, question_max_length)
         context_data, context_mask = pad(context_embed, n_features, context_max_length)
 
-        ret.append((question_data, context_data, 
-                    answer_labels, 
+        ret.append((question_data, context_data,
+                    answer_labels,
                     question_mask, context_mask))
 
     return ret
@@ -47,7 +47,7 @@ def embed(tokens, embed_dict):
         word = normalize(token)
         # word's embedding (UNK's embedding otherwise)
         wv = embed_dict.get(word, embed_dict[UNK])
-        
+
         ret.append(wv)
 
     return ret
@@ -56,24 +56,26 @@ def normalize(word):
     """
     Normalize words that are numbers or have casing.
     """
-    if word.isdigit(): 
+    if word.isdigit():
         return NUM
-    else: 
+    else:
         return word.lower()
 
 def labelize(span, max_length):
     # create negative label list
     ml = max_length
-    labels = LBLS[-1] * ml
+    labels = [None]* ml
     # set appropriate labels positive
     # if span[0] < ml:
     #     start = span[0]
     #     end = min(span[1] + 1, ml)
     #     labels[start:end] = LBLS[0] * (end - start)
-    if span[0]< ml: 
+    if span[0]< ml:
         labels[span[0]] = LBLS[0]
-    if span[1]< ml: 
+        labels[span[1]] = LBLS[-1]
+    if span[1]< ml:
         labels[span[1]] = LBLS[1]
+        labels[span[0]] = LBLS[-1]
 
     assert len(labels) == max_length, "Labelize: 'labels' is of the wrong shape!"
 
@@ -82,7 +84,7 @@ def labelize(span, max_length):
 def pad(word_vector, n_features, max_length):
     # initialize padding variables
     zero_vector = [0] * n_features
-    zero_label = 0 
+    zero_label = 0
     # pad word_vector to max_length
     pad_len = max(max_length - len(word_vector), 0)
     padding = zero_vector * pad_len
